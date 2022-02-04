19ffb537863ad7f3534334d16f0559ea2ae4c5eb
Pauline Millet <milletp.pro@gmail.com>
2022-02-04 10:58:55 +0100

    working on exam 3

--- exam03/micro_paint/micro_paint.c
+++ exam03/micro_paint/micro_paint.c
@@ -3,29 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   micro_paint.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nico <nico@student.42lyon.fr>              +#+  +:+       +#+        */
+/*   By: pmillet <milletp.pro@gmail.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/05 22:41:22 by gbaud             #+#    #+#             */
-/*   Updated: 2021/12/01 15:30:45 by nico             ###   ########lyon.fr   */
+/*   Updated: 2022/02/02 09:47:22 by pmillet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
-#include <math.h>
-#include <limits.h>
 
 #define ERR_ARG "Error: argument\n"
 #define ERR_OPE "Error: Operation file corrupted\n"
 
 typedef struct  s_rect 
 {
-    char        t;
-    char        f;  // tft sans le t
-    float       x;
-    float       y;
-    float       w;
-    float       h;
+    char        t; // type : border ou fill ?
+    char        f; // filling char
+    float       x; // x pos of the painted rect
+    float       y; // y pos of the painted rect
+    float       w; // width of canevas
+    float       h; // height of canevas
 }               t_rect;
 
 int ft_strlen(char *str) {
@@ -42,23 +40,24 @@ int put_error(char *err) {
     return (1);
 }
 
-int is_border(float x, float y, t_rect r) {  // est on sur le bord du rectangle a imprimer ?
+int is_border(float x, float y, t_rect r) {
 // est-on a l'interieur du rectangle a ecrire ?
-    if (x < r.x || x > r.x + r.w ||  // est-on dans la zone du rectangle a ecrire ? (trop a gauche : x < r.x, ou trop a droite)
+    if (x < r.x || x > r.x + r.w ||  // est-on trop a gauche ? x < r.x || la shape est elle assez large ?)
         y < r.y || y > r.y + r.h) // meme ligne mais pour la hauteur
         return (0);
 // est-on sur le bord du rectangle a ecrire ?
     if ((x - r.x < 1.00000000 || (r.x + r.w) - x < 1.00000000) || // est-on sur le bord droit du rectangle a ecrire/le bord du canevas ?
         (y - r.y < 1.00000000 || (r.y + r.h) - y < 1.00000000)) // meme ligne mais en inversé, pour la verticale
         return (2);
-// est-on ailleurs ?
+// est-on ailleurs ? (hors rect)
     return (1);
 }
 
-int put_rect(int w, int h, char canvas[h][w], t_rect r) // affiche le rectangle a ecrire sur le caneva 
+int put_rect(int w, int h, char canvas[h][w], t_rect r) // dessine le rectangle a ecrire sur le canevas en memoire
 {
     for (int y = 0; y < h; y++)
         for (int x = 0; x < w; x++)
+        // bordure (r) ou rect plein (R) ?
             if (r.t == 'r') 
             {
                 if (is_border(x, y, r) == 2)
@@ -66,7 +65,7 @@ int put_rect(int w, int h, char canvas[h][w], t_rect r) // affiche le rectangle 
             } 
             else if (r.t == 'R')
             {
-                if (is_border(x, y, r) > 0) // meme chose, juste une différence au niveau de la comparaison
+                if (is_border(x, y, r) > 0) // meme chose, juste une différence au niveau de la comparaison de la return value
                     canvas[y][x] = r.f;
             } 
             else 
@@ -84,43 +83,43 @@ int main(int ac, char **av) {
 
     if (ac != 2)
         return (put_error(ERR_ARG));
-    if (!(operation_file = fopen(av[1], "r"))) // ouvre le fichier
+    if (!(operation_file = fopen(av[1], "r"))) // ouvre le fichier en read
         return (put_error(ERR_OPE));
 
+    // on check la 1ere ligne du fd : taille et char du canevas
     last = fscanf(operation_file, "%d %d %c\n", &w, &h, &c);
     if (last != 3 || w <= 0 || w > 300 || h <= 0 || h > 300) // si last n'est pas egale a 3, une conversion a foiré, 0 < W et H < 300
         return (put_error(ERR_OPE));
-        
-    // parsing stop
-    // on remplit le fond du canevas
+    
+    // parsing step 1 stop
+    // on cree et remplit le fond du canevas
     char canvas[h][w];
     for (int y = 0; y < h; y++)
         for (int x = 0; x < w; x++)
             canvas[y][x] = c;
 
-    // parsing start
-    
+    // parsing step 2 start
     while (1)
     {
         last = fscanf(operation_file, "%c %f %f %f %f %c\n", &r.t, &r.x, &r.y, &r.w, &r.h, &r.f); // le f est en dernier
      
         if (last == EOF)
             break;
-        else if (last != 6 || r.w <= 0 || r.h <= 0) // meme parsing que plus haut mais juste un check des arg et si plus grand que 0
+        else if (last != 6 || r.w <= 0 || r.h <= 0) // meme parsing que plus haut mais juste un check des arg et si w et h st plus grands que 0
             return (put_error(ERR_OPE));
     // parsing stop
     // on ecrit le rectangle
-        else if (put_rect(w, h, canvas, r))  // 0 = rien a signaler
+        else if (put_rect(w, h, canvas, r))  // On ecrit le rect parsé a la ligne courante. 0 = rien a signaler
             return (put_error(ERR_OPE));
     }
     
-    for (int y = 0; y < h; y++) // print du canvas vers la sortie standard
+    for (int y = 0; y < h; y++) // print du canvas de la memoire vers la sortie standard, ligne par ligne
     {
         write(1, canvas[y], w);
         write(1, "\n", 1);
     }
     
-    fclose(operation_file); // ne pas oublier
+    fclose(operation_file); // ne pas oublier !!!!
     
     return (0);
 }
---
+++ exam03/train/mini_paint.c
@@ -0,0 +1,115 @@
+#include <math.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+
+#define ERR_ARG "Error: argument"
+#define ERR_OPE "Error: Operation file corrupted"
+
+typedef struct	s_circ
+{
+	char	t;
+	char	f;
+	float	x;
+	float	y;
+	float	r;
+}				t_circ;
+
+int ft_strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	put_error(char *err)
+{
+	write(1, err, ft_strlen(err));
+	return (1);
+}
+
+int dist(float x1, float y1, float x2, float y2)
+{
+	return(sqrtf(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2))));
+	// return(sqrtf(((x1 - x2) * (x1 - x2)) + ((y1 - y2) * (y1 - y2))));
+	// return (sqrtf(powf(x2 - x1, 2) + powf(y2 - y1, 2)));
+}
+
+int is_border(float x, float y, t_circ r)
+{
+	if (dist(x, y, r.x, r.y) > r.r)
+		return (0);
+	if (dist(x, y, r.x, r.y) > r.r - 1)
+		return (2);
+	return (1);
+}
+
+int	put_circ(int w, int h, char canvas[h][w], t_circ r)
+{
+	for (int y = 0; y < h; y++)
+	{
+		for (int x = 0; x < w; x++)
+		{
+			if (r.t == 'c')
+			{
+				if (is_border(x, y, r) == 2)
+					canvas[y][x] = r.f;
+			}
+			else if (r.t == 'C')
+			{
+				if (is_border(x, y, r) > 0)
+					canvas[y][x] = r.f;
+			}
+			else
+				return(1);
+		}
+	}
+	return (0);
+}
+
+int main (int ac, char **av)
+{
+	FILE *operation_file;
+	int w, h, last;
+	char c;
+	t_circ r;
+
+	if (ac != 2)
+		return(put_error(ERR_ARG));
+	if (!(operation_file = fopen(av[1], "r")))
+		return(put_error(ERR_OPE));
+
+	last = fscanf(operation_file, "%d %d %c\n", &w, &h, &c);
+	if (last != 3 || w <= 0 || w > 300 || h <= 0 || h > 300)
+		return(put_error(ERR_OPE));
+
+	char canvas[h][w];
+	for (int y = 0; y < h; y++)
+		for (int x = 0; x < w; x++)
+			canvas[y][x] = c;
+
+	while (1)
+	{
+		last = fscanf(operation_file, "%c %f %f %f %c\n", &r.t, &r.x, &r.y, &r.r, &r.f);
+		if (last == EOF)
+			break;
+		if (last != 5 || r.r <= 0)
+		{
+			return(put_error(ERR_OPE));
+		}
+		else if (put_circ(w, h, canvas, r))
+		{
+			return(put_error(ERR_OPE));
+		}
+	}
+
+	for (int y = 0; y < h; y++)
+	{
+		write(1, canvas[y], w);
+		write(1, "\n", 1);
+	}
+	fclose(operation_file);
+	return (0);
+}
